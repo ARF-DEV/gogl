48cdf7e59bee1d0f8f2c697a8164c6c0adca14a4
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2023-04-23 10:57:50 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ARF-DEV/gogl
+
+go 1.20
+
+require github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
---
+++ gogl.go
@@ -0,0 +1,29 @@
+package gogl
+
+import (
+	"errors"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+func GetVersion() string {
+	return gl.GoStr(gl.GetString(gl.VERSION))
+}
+
+func CompileShader(shaderSource string, shaderType uint32) (uint32, error) {
+	shaderId := gl.CreateShader(shaderType)
+	cSource, free := gl.Strs(shaderSource)
+	gl.ShaderSource(shaderId, 1, cSource, nil)
+	free()
+	gl.CompileShader(shaderId)
+	var glErr int32
+	gl.GetShaderiv(shaderId, gl.COMPILE_STATUS, &glErr)
+	if glErr == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shaderId, gl.INFO_LOG_LENGTH, &logLength)
+		log := string(make([]byte, logLength+1))
+		gl.GetShaderInfoLog(shaderId, logLength, nil, gl.Str(log))
+		return 0, errors.New("Error Failed to compile shader:\n" + log)
+	}
+	return shaderId, nil
+}
